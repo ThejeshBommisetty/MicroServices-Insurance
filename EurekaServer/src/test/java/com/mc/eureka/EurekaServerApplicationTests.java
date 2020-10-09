2bc462c8eeaf51005ba20c251deff057aa71cfff
Thejesh Bommisetty <47267710+ThejeshBommisetty@users.noreply.github.com>
2020-10-09 19:01:45 +0530

    Eureka Server
    For service Discovery

---
+++ EurekaServer/src/test/java/com/mc/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
